cf06d9095084a928915ba787329b664f86e0bc5e
JaseemKhan211 <jskhan211@gmail.com>
2025-11-23 12:39:31 +0500

    🧩 Update: Image Uploader Using Multers

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,6 +62,9 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.body);
+  console.log(req.file);
+
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -89,6 +92,35 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+// exports.updateMe = catchAsync(async (req, res, next) => {
+//   // 1) Create error if user POSTs password data
+//   if (req.body.password || req.body.passwordConfirm) {
+//     return next(
+//       new AppError(
+//         'This route is not for password updates. Please use /updateMyPassword.',
+//         400
+//       )
+//     );
+//   }
+
+//   // 2) Filtered out unwanted fields names that are not allowed to be updated
+//   const filteredBody = filterObj(req.body, 'username', 'email');
+//   if (req.file) filteredBody.photo = req.file.filename;
+
+//   // 3) Update user document
+//   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+//     new: true,
+//     runValidators: true
+//   });
+
+//   res.status(200).json({
+//     status: 'success',
+//     data: {
+//       user: updatedUser
+//     }
+//   });
+// });
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
--- routes/userRouters.js
+++ routes/userRouters.js
@@ -6,27 +6,36 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.get('/logout', authController.logout);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.post('/resetPassword', authController.resetPassword);
 
-router.get('/logout', authController.logout);
-router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
-router.get('/me', userController.getMe, userController.getUser);
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', 
+    userController.getMe, 
+    userController.getUser);
+
 router.patch(
     '/updateMe', 
     userController.uploadUserPhoto,
     userController.resizeUserPhoto, 
     userController.updateMe
 );
+
 router.delete('/deleteMe', userController.deleteMe);
 
 router
     .route('/')
-    .get(authController.protect, userController.getAllUsers);
+    .get(userController.getAllUsers);
 
 router
     .route('/:id')
     .get(userController.getUser)
     .delete(authController.protect, authController.restrictTo('admin'), userController.deleteUser);
-module.exports = router;
+
+    module.exports = router;
