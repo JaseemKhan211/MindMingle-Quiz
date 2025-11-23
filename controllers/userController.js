8c2c219e155fa48f8aac9321ae6b6ad270aca3fc
JaseemKhan211 <jskhan211@gmail.com>
2025-11-23 14:48:44 +0500

    🧩 Update: Configuring Multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,17 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.memoryStorage();
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  }
+});
+
+// const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -15,7 +25,7 @@ const multerFilter = (req, file, cb) => {
   }
 };
 
-const upload = multer({
+const upload = multer({ 
   storage: multerStorage,
   fileFilter: multerFilter
 });
@@ -62,8 +72,8 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.body);
   console.log(req.file);
+  console.log(req.body);
 
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -92,35 +102,6 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     }
   });
 });
-
-// exports.updateMe = catchAsync(async (req, res, next) => {
-//   // 1) Create error if user POSTs password data
-//   if (req.body.password || req.body.passwordConfirm) {
-//     return next(
-//       new AppError(
-//         'This route is not for password updates. Please use /updateMyPassword.',
-//         400
-//       )
-//     );
-//   }
-
-//   // 2) Filtered out unwanted fields names that are not allowed to be updated
-//   const filteredBody = filterObj(req.body, 'username', 'email');
-//   if (req.file) filteredBody.photo = req.file.filename;
-
-//   // 3) Update user document
-//   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-//     new: true,
-//     runValidators: true
-//   });
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       user: updatedUser
-//     }
-//   });
-// });
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
--- routes/userRouters.js
+++ routes/userRouters.js
@@ -23,7 +23,7 @@ router.get('/me',
 router.patch(
     '/updateMe', 
     userController.uploadUserPhoto,
-    userController.resizeUserPhoto, 
+    // userController.resizeUserPhoto, 
     userController.updateMe
 );
 
