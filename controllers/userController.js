fa8ee1aa74543409d469dff9c42e022c5a91e0ad
JaseemKhan211 <jskhan211@gmail.com>
2025-12-01 21:41:32 +0500

    🗃️ Updated: Manage users roles report add role column select list

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,6 +66,10 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     });
 });
 
+exports.updateRole = catchAsync(async (req, res, next) => {
+  
+});
+
 exports.getMe = (req, res, next) => {
     req.params.id = req.user.id;
     next();
